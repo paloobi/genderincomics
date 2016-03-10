779c87e24d4dc2ef5394d0454e6fea0a81dafbae
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-10 16:48:50 -0500

    pie chart added of overall representation

--- browser/js/common/directives/stats/stats.js
+++ browser/js/common/directives/stats/stats.js
@@ -21,7 +21,45 @@ app.directive('stats', function(StatFactory) {
       scope.stats = {};
       StatFactory.getOverall().then(function(stats){
         scope.stats.overall = stats;
+        makePieChart(stats);
       })
     }
   }
 });
+
+function makePieChart(stats) {
+
+  function getData(stats) {
+    var data = [];
+
+    for (var stat in stats) {
+      if (['female', 'male', 'other'].indexOf(stat) > -1) {
+        data.push({
+          "label": stat,
+          "value": stats[stat]
+        })
+      }
+    }
+    return data;
+  }
+
+  var height = 600;
+  var width = 600;
+
+  nv.addGraph(function() {
+
+    var chart = nv.models.pieChart()
+        .x(function(d) { return d.label })
+        .y(function(d) { return d.value })
+        .width(width)
+        .height(height)
+
+    d3.select("#overall")
+          .datum(getData(stats))
+          .transition().duration(350)
+          .call(chart);
+
+    return chart;
+  });
+
+}
