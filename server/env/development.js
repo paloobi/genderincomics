94a4a07f424ab87c052ffac8e2d0e028299ed3d6
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-12 17:01:04 -0500

    removing unnecessary files

--- server/env/development.js
+++ server/env/development.js
@@ -1,21 +1,6 @@
 module.exports = {
   "DATABASE_URI": "mongodb://localhost:27017/comicGender",
   "SESSION_SECRET": "Optimus Prime is my real dad",
-  "TWITTER": {
-    "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
-    "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
-    "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
-  },
-  "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
-  },
-  "GOOGLE": {
-    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
-    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
-  },
   "COMIC_VINE": {
     "API_KEY": "48f39272e4a344376bf34fb0659c0d97e82205aa",
     "BASE_URL": "http://comicvine.gamespot.com/api"
