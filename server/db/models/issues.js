c4c7a519fb9f66b81879d78d2280e37c087486d9
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-12 19:49:44 -0500

    numberof issues generator

--- server/db/models/issues.js
+++ server/db/models/issues.js
@@ -20,27 +20,54 @@ var schema = new mongoose.Schema({
     }    
 });
 
-schema.statics.calculate = function (publisher) {
-    var stats = {publisher: publisher};
-    Character.find({publisher: publisher})
-    .then(function(characters){
-        var numCharacters = characters.length;
-        var numIssues = characters.reduce(function(sum, character){
-            return sum + character.issueCount;
-        }, 0)
-        var average = numIssues / numCharacters;
-        return this.find({publisher: publisher})
-    })
-    .then(function(stat) {
-        if (!stat) return this.create(stats);
-        else {
-            stat.set(stats);
-            return stat.save();
+// used to seed DB with calculations from Character model
+schema.statics.calculate = function () {
+    var stats = {};
+    Character.find().then(function(characters){
+        characters.forEach(function (char) {
+
+            // add to overall stats
+            if (!stats.overall) stats[overall] = { publisher: 'overall' };
+            if (!stats.overall[char.gender]) stats.overall[char.gender] = { sum: 0, dataPoints: 0 };
+            stats.overall[char.gender].sum += char.issueCount;
+            stats.overall[char.gender].dataPoints++
+
+            // add to publisher tally
+            if (char.publisher) {
+                if (!stats[char.publisher]) stats[char.publisher] = { publisher: char.publisher };
+                if (!stats[char.publisher][char.gender]) stats[char.publisher][char.gender] = {sum: 0, dataPoints: 0}
+                stats[char.publisher][char.gender].sum += char.issueCount;
+                stats[char.publisher][char.gender].dataPoints++;
+            }
+
+        });
+
+        var dbPromises = [];
+        for (var stat in stats) {
+            // calculate averages
+            for (var gender in stats[stat]) {
+                if (gender !== 'publisher') stats[stat][gender] = stats[stat][gender].sum / stats[stat][gender].dataPoints;
+            }
+            // promise to save to DB
+            dbPromises.push( this.create(stats[stat]) );
         }
-    }
+        return Promise.all(dbPromises);
+    })
+    .then(function() {
+        console.log(chalk.green("Successfully calculated and seeded new percents"))
+    })
+    .catch(function(err) {
+        console.log(chalk.red("Failed to Save to DB"));
+    })
 }
 
-schema.statics.getByPublisher = counters.getByPublisher;
+schema.statics.getOverall = function() {
+    return this.findOne({publisher: 'overall'});
+}
+
+schema.statics.getByPublisher = function(publisher) {
+    return this.findOne({publisher: publisher})
+}
 
 mongoose.model('GenderCount', schema);
 
